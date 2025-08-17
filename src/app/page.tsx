c881e2f74159dd18e14ca0cb190854ef4b5d0b88
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-08-17 21:58:28 +1000

    fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Values_1 from "@/components/features/values/Values_1";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/navigation/Navbar";
 
+import Navbar from "@/components/navigation/Navbar";
 export default function Home() {
   return (
     <main className="overflow-hidden">
