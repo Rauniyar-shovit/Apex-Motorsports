810684ab698c555f066616db2892aae38b0f3064
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-08-17 21:45:49 +1000

    made a responsive value section

--- src/components/features/values/Values.tsx
+++ src/components/features/values/Values.tsx
@@ -14,21 +14,21 @@ const features = [
 const Values = () => {
   return (
     <section id="values">
-      <div className="pt-20 lg:pt-32 w-[90%] lg:w-[78%] 2xl:w-[50%] mx-auto">
-        <div className="items-center flex flex-col h-145 2xl:h-160 ">
-          <div className="flex-1 w-full h-full relative">
-            {/* <div className="relative w-[75%] 2xl:w-[80%] h-full "> */}
-            <div className="relative lg:w-[75%] h-full ">
+      <div className="pt-20 md:pt-32 w-[90%] xl:w-[78%] 2xl:w-[50%] mx-auto">
+        <div className="items-center flex flex-col md:flex-row md:h-145 2xl:h-160 gap-6 md:gap-15">
+          <div className="flex flex-col md:flex-1 w-full h-full relative">
+            <div className="relative w-full md:w-[80%]  h-[450px] lg:h-full ">
+              {/* <div className="relative w-full md:w-[75%] h-full "> */}
               <Image
                 src={"/value_1.avif"}
                 alt="value"
-                className="absolute w-full h-full object-cover"
-                width={600}
-                height={1000}
+                className="absolute w-full h-full "
+                width={1000}
+                height={1200}
               />
             </div>
-            <div className="absolute bottom-0 lg:-bottom-1/6 lg:-right-10 max-h-80 lg:max-h-none lg:min-h-106 lg:w-106 2xl:w-120 items-center justify-center  bg-foreground  p-4 lg:p-6 text-white shadow-2xl group">
-              <div className="  p-6 2xl:p-10 flex flex-col justify-between cursor-pointer">
+            <div className="md:absolute bottom-0  md:bottom-4 lg:-bottom-1/6  md:-right-10 max-h-80 lg:max-h-none md:min-h-106 md:w-75 lg:w-106 2xl:w-120 items-center justify-center  bg-foreground  p-4 lg:p-6 text-white shadow-2xl group">
+              <div className="p-6 2xl:p-10 flex flex-col justify-between cursor-pointer">
                 <IoChatbubblesOutline className="h-15 w-15 mb-8 lg:mb-14" />
                 <p className="font-sans text-muted-secondary  lg:text-lg mb-6 lg:mb-10">
                   Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
@@ -42,17 +42,17 @@ const Values = () => {
           </div>
 
           <div className="flex-1 w-full h-full">
-            <div className="lg:pl-15 flex flex-col justify-between h-full ">
-              <p className="hidden lg:block text-lg font-sans text-muted-primary">
+            <div className="md:pl-8 lg:pl-15 flex flex-col justify-between h-full ">
+              <p className="hidden md:block text-lg font-sans text-muted-primary">
                 Adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
                 Wuismod tempor incidunt
               </p>
-              <div className="my-10 lg:mb-4">
+              <div className="my-4 md:my-10 ">
                 {features.map((item, index) => (
                   <p
                     key={index}
-                    className="font-bold text-2xl lg:text-[2.8rem]  font-barlow text-gray-500 uppercase mb-1"
+                    className="font-bold text-2xl xl:text-[2.8rem]  font-barlow text-gray-500 uppercase mb-1 lg:mb-3"
                   >
                     {item}
                   </p>
