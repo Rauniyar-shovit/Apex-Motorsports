a66f36b5c1b0f7ea9fc9429e9659c0e4088841c1
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-08-22 12:21:11 +1000

    added the reveal animation to contact form

--- src/components/features/contact/ContactForm.tsx
+++ src/components/features/contact/ContactForm.tsx
@@ -4,14 +4,22 @@ import React from "react";
 import InputField from "./InputField";
 import { BsSend } from "react-icons/bs";
 import { FaEnvelope, FaPencil, FaUser } from "react-icons/fa6";
+import RevealWrapper from "@/components/RevealWrapper";
 
 const ContactForm = () => {
   return (
     <div className="flex-1 bg-form-background 2xl:mx-10 w-full">
       <div className="px-6 py-16 md:px-8 md:py-24 lg:px-14 lg:py-28 2xl:px-18">
-        <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-barlow uppercase mb-10">
-          Contact Form
-        </h2>
+        <RevealWrapper
+          variants={{
+            hidden: { opacity: 0, x: -75 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-barlow uppercase mb-10">
+            Contact Form
+          </h2>
+        </RevealWrapper>
         <div className="flex flex-col gap-4 md:gap-8 mb-16 ">
           <InputField label={"Name"} icon={FaUser} />
           <InputField label={"Email Address"} icon={FaEnvelope} />
