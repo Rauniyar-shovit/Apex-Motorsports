9add9069e49cb996d55125f9b4403e54c358a134
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-08-22 20:02:58 +1000

    fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "@/components/navigation/Navbar";
 import Achievements from "@/components/features/achievements/Achievements";
 import Contact from "@/components/features/contact/Contact";
 import Footer from "@/components/footer/Footer";
-import Partners from "@/components/features/partners/Partners";
 import MarqueeText from "@/components/features/marquee/MarqueeText";
 import MarqueePartners from "@/components/features/marquee/MarqueePartners";
 
