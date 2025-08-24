ba25e5559a43886a0344ca2faa16bdb1e6771a49
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-08-24 19:41:59 +1000

    added the dummy text

--- src/components/features/whoAreWe/DarkPanel.tsx
+++ src/components/features/whoAreWe/DarkPanel.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 
 const DarkPanel = () => {
+  const containerPaddings =
+    "px-6 py-12 lg:px-8 sm:py-16 md:py-12 xl:p-16 3xl:px-24 ";
   return (
-    <div className="mx-auto max-w-4xl px-6 lg:px-8 py-16 space-y-12 bg-foreground text-white font-sans">
-      {/* What We Do */}
+    <div
+      className={`${containerPaddings} space-y-8 md:space-y-12 bg-foreground font-sans text-white h-full flex flex-col  justify-center`}
+    >
       <div>
-        <h3 className="uppercase font-barlow text-xl font-extrabold mb-3">
+        <h3 className="uppercase font-barlow text-xl xl:text-2xl font-extrabold mb-1 md:mb-3">
           What We Do
         </h3>
-        <p className="text-white/80 leading-relaxed">
-          Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua enim ad.
+        <p className="text-muted-secondary leading-relaxed ">
+          We design, build, and race formula-style cars, turning classroom
+          learning into real engineering experience.
         </p>
       </div>
 
       {/* The Opportunities */}
       <div>
-        <h3 className="uppercase font-barlow text-xl font-extrabold mb-3">
+        <h3 className="uppercase font-barlow text-xl xl:text-2xl font-extrabold mb-1 md:mb-3">
           The Opportunities
         </h3>
-        <p className="text-white/80 leading-relaxed">
-          Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua enim ad.
+        <p className="text-muted-secondary leading-relaxed ">
+          The competition lets us innovate, collaborate, and gain industry-ready
+          skills in engineering, leadership, and teamwork.
         </p>
       </div>
     </div>
--- src/components/features/whoAreWe/WhoAreWe.tsx
+++ src/components/features/whoAreWe/WhoAreWe.tsx
@@ -7,15 +7,13 @@ import InfoSection from "./InfoSection";
 export default function WhoAreWe() {
   return (
     <section className="relative section-padding">
-      {/* Top row */}
-      <div className="grid grid-cols-1 md:grid-cols-3  md:grid-rows-2 items-start">
+      <div className="grid grid-cols-1 md:grid-cols-3  md:[grid-template-rows:auto_auto] items-start">
         <InfoSection />
 
-        <div className=" md:row-start-2 md:col-start-1  md:col-span-1 relative bg-red-500 h-full w-full">
+        <div className=" md:row-start-2 md:col-start-1  md:col-span-1 relative bg-red-500 2xl:min-h-[430px] 3xl:min-h-[480px] h-full w-full">
           <Image
             src={"/engineering-kart.avif"}
-            width={700}
-            height={800}
+            fill
             alt="model"
             objectFit="cover"
           />
@@ -23,14 +21,7 @@ export default function WhoAreWe() {
 
         <DarkPanel />
         <div className=" md:row-span-full md:col-start-3  md:col-span-1 relative bg-red-500 h-full w-full">
-          <Image
-            src={"/model.avif"}
-            // width={500}
-            // height={1000}
-            fill
-            alt="model"
-            objectFit="cover"
-          />
+          <Image src={"/model.avif"} fill alt="model" objectFit="cover" />
         </div>
       </div>
     </section>
