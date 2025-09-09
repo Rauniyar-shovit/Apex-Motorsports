5e30b4c576df0a51b2e2f53b9171e8b7bdcf7641
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-09-09 13:33:04 +1000

    added the email hover btn in profile card

--- src/app/(sites)/(unshared-layout)/our_team/_components/ProfileCard.tsx
+++ src/app/(sites)/(unshared-layout)/our_team/_components/ProfileCard.tsx
@@ -1,11 +1,12 @@
+import Button from "@/components/reusable/Button";
 import { TeamMemberProfile } from "@/models";
 import Image from "next/image";
-import React from "react";
+import { IoMailOutline } from "react-icons/io5";
 
-const ProfileCard = ({ name, photo, role, bio }: TeamMemberProfile) => {
+const ProfileCard = ({ name, photo, role, bio, email }: TeamMemberProfile) => {
   return (
     <div className="group md:max-w-[260px] w-full">
-      <div className="relative h-64 w-full overflow-hidden ">
+      <div className="relative h-64 w-full overflow-hidden">
         <Image
           src={photo}
           alt={name}
@@ -13,12 +14,26 @@ const ProfileCard = ({ name, photo, role, bio }: TeamMemberProfile) => {
           className="object-cover"
           sizes="(max-width: 1024px) 50vw, 25vw"
         />
+
+        {/* Hover overlay with email button */}
+        <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          <a href={`mailto:${email}`} className="inline-block">
+            <Button
+              btnType="submit"
+              title={"Email"}
+              icon={IoMailOutline}
+              btnStyles="px-6 py-2 md:px-6 md:py-4  bg-foreground  text-white mt-16 hover:text-foreground "
+              beforeStyles="before:bg-white"
+              btnTextStyles="text-xs"
+            />
+          </a>
+        </div>
       </div>
 
-      <div className="mt-4 ">
-        <h3 className="text-[15px] font-semibold text-foreground ">{name}</h3>
+      <div className="mt-4">
+        <h3 className="text-[15px] font-semibold text-foreground">{name}</h3>
         <p className="mt-1 text-sm text-muted-primary">{role}</p>
-        <p className="mt-3 text-sm text-muted-primary ">{bio}</p>
+        <p className="mt-3 text-sm text-muted-primary">{bio}</p>
       </div>
     </div>
   );
