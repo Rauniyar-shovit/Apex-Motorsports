24757e0466ceb01af3b0b33957235385e7b8832b
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-09-28 21:45:06 +1000

    added the alumni page and parallax

---
+++ src/app/(sites)/(shared-breadcrumbs)/alumni/page.tsx
@@ -0,0 +1,35 @@
+import raceTrack from "@/../../public/race-track.jpg";
+import ParallaxContainer from "@/components/features/achievements/ParallaxContainer";
+
+const page = () => {
+  return (
+    <>
+      <section className="px-6 py-16">
+        <div className="font-sans max-w-4xl 2xl:max-w-5xl mx-auto text-center">
+          <p className="uppercase text-sm font-[600] tracking-wider mb-4 ">
+            CELEBRATING OUR LEGACY
+          </p>
+          <p className=" font-sans text-center  text-base md:text-lg leading-relaxed">
+            Our alumni are the heart of Apex Motorsport’s story. They carried
+            the lessons of teamwork, innovation, and resilience beyond the track
+            and into their professional careers. Each achievement they’ve earned
+            continues to inspire current and future members, reminding us that
+            the Apex legacy never stops growing.
+          </p>
+        </div>
+        <div className="mt-6 border-b-2 border-primary w-40 mx-auto" />
+      </section>
+      <ParallaxContainer
+        bgImagePath={raceTrack}
+        bgImageTitle={"race track"}
+        containerStyles="h-[40vh] 3xl:h-[35vh]"
+      >
+        <div className="wrapper flex items-center justify-center text-center uppercase text-6xl font-barlow h-full">
+          Meet Our Alumnis
+        </div>
+      </ParallaxContainer>
+    </>
+  );
+};
+
+export default page;
