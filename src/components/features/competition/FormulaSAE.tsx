8139ac63506708ab20c67add17d8cc7892150fc6
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-11-03 15:10:16 +1100

    added the points pie chart

--- src/components/features/competition/FormulaSAE.tsx
+++ src/components/features/competition/FormulaSAE.tsx
@@ -2,6 +2,7 @@
 import { FORMULA_SAE_DETAILS } from "@/constants";
 import CompetitionInfo from "./CompetitionInfo";
 import StatsCard from "./StatsCard";
+import { PointsChart } from "./PointsChart";
 
 export default function FormulaSAE() {
   return (
@@ -28,8 +29,31 @@ export default function FormulaSAE() {
             title={FORMULA_SAE_DETAILS.stats.students.title}
           />
         </div>
-        <div>
-
+        <div className=" flex flex-col items-center gap-4 md:flex-row mt-10 ">
+          <PointsChart />
+          <div className="flex-1 font-sans">
+            <p className="uppercase font-barlow font-[600]   text-2xl mb-4">
+              Competition Structure
+            </p>
+            <p className="mb-4">
+              Formula Student is divided into distinct classes; Combustion,
+              Driverless and Electric. At competitions each car competes
+              separately and is judged in two types of events, Dynamic and
+              Static.
+            </p>
+            <p className="mb-4">
+              Static events allow the team to demonstrate their knowledge behind
+              the development of the cars and test industry skills such as
+              entrepreneurialism, financial acumen and the holistic engineering
+              design process behind the cars.
+            </p>
+            <p className="mb-4">
+              Dynamic events are the culmination of the design process, testing
+              the performance of the cars in a variety of scenarios. These
+              events require more than just the quickest car; they are a test of
+              reliability, efficiency and driver skill.
+            </p>
+          </div>
         </div>
       </div>
     </section>
